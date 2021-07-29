26e46d3a34f363cb4d0fdaf3312473c7fd996ee7
zarm23 <arnaldo.rmartinez@gmail.com>
2021-07-28 21:25:31 -0300

    cambios en mirama

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from portfolio import views as portfolio_views
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name="about"),
-    path('portfolio/', views.portfolio, name="portfolio"),
+    path('portfolio/', portfolio_views.portfolio, name="portfolio"),
     path('contact/', views.contact, name="contact"),
 ]
--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Título")
     description = models.TextField(verbose_name="Descripción")
     image = models.ImageField(upload_to="projects", verbose_name="Imagen")
+    link = models.URLField(null=True, blank=True, verbose_name="Dirección Web")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
 
